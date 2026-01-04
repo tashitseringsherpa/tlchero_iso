83c4a148d77b8d140c29887419e66d7050aa882b
tashi <test@gmail.com>
2026-01-03 22:06:32 -0500

    Initial Commit

---
+++ TLC HEROTests/TLC_HEROTests.swift
@@ -0,0 +1,17 @@
+//
+//  TLC_HEROTests.swift
+//  TLC HEROTests
+//
+//  Created by Tashi Sherpa on 1/3/26.
+//
+
+import Testing
+@testable import TLC_HERO
+
+struct TLC_HEROTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
